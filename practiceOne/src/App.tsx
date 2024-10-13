0831838d66503e5e2785ea3a2d224203ec5164d1
amir <amirhossin.es2004@gmail.com>
2024-10-13 22:50:45 +0330

    last update

--- practiceOne/src/App.tsx
+++ practiceOne/src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
 // import TestComponent from "./component/ReactQuery/TestQuery/Test";
-import AppQuery from "./component/ReactQuery/AppQuery";
+// import AppQuery from "./component/ReactQuery/AppQuery";
 // import AppTheme from "./component/useContext/Theme/AppTheme";
 // import AppZustand from "./component/zustand/AppZustand";
 function App() {
-  return (
-    <>
-      <AppQuery />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
